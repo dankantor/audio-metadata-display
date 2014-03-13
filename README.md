7072fe7ae7b6d7a90a1f78d25c0b51bbd0a9109b
Dan Kantor <dan.kantor@gmail.com>
2014-03-13 15:43:55 -0400

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-audio-metadata-display
+Audio Metadata Display
 ======================
 
 Uses PlayQueue to display coverart, title, artist and album html
+
+## Install
+
+    npm install audio-metadata-controls
+
+
+## Usage
+
+    var AudioMetadataControls = require('audio-metadata-controls');
+    var audioControls = new AudioMetadataControls(
+        {
+            'playQueue': this.playQueue,
+            'coverart': '#song-coverart',
+            'title': '#song-title',
+            'artist': '#song-artist'
+        }
+    );
+    
+## Example HTML
+
+    <div class="song-metadata">
+        <div class="song-coverart" id="song-coverart"></div>
+        <div class="song-title" id="song-title"></div>
+        <div class="song-artist" id="song-artist"></div>
+        <div class="song-album" id="song-album"></div>
+    </div>
+    
+## Example CSS
+
+    .song-metadata {
+        position: absolute;
+        top: 30px;
+        left: 110px;
+        right: 0;
+        height: 40px;
+    }
+    .song-coverart {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        height: 50px;
+        width: 50px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-color: #F3F3F3;
+        background-size: contain;
+    }
+    .song-title {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        text-align: center;
+        font-size: 14px;
+        .ellipsis();
+    }
+    .song-artist {
+        position: absolute;
+        top: 18px;
+        left: 0;
+        right: 0;
+        text-align: center;
+        font-size: 12px;
+        .ellipsis();
+    }
+    .song-album {
+        position: absolute;
+        top: 28px;
+        left: 0;
+        right: 0;
+        text-align: center;
+        font-size: 12px;
+        .ellipsis();
+    }
+
+
---
+++ index.js
@@ -0,0 +1,109 @@
+(function(){
+
+// constructor
+function AudioMetadataDisplay(opts){
+    this.playQueue = null;
+    if(opts.playQueue){
+        this.playQueue = opts.playQueue;
+    }
+    this.songMap = {
+        'coverart': 'coverart',
+        'title': 'title',
+        'artist': 'artist',
+        'album': 'album'
+    }
+    this.cacheElements(opts);
+    this.addListeners();
+    this.setInitital = opts.setInitial || true;
+    if(this.setInitital === true){
+        if(this.playQueue !== null){
+            var song = this.playQueue.getSong();
+            this.set(song);   
+        }
+    }
+}
+
+AudioMetadataDisplay.prototype.cacheElements = function(opts){
+    if(opts.coverart){
+        this.coverartEl = $(opts.coverart);
+    }
+    if(opts.title){
+        this.titleEl = $(opts.title);
+    }
+    if(opts.artist){
+        this.artistEl = $(opts.artist);
+    }
+    if(opts.album){
+        this.albumEl = $(opts.album);
+    }
+}
+
+AudioMetadataDisplay.prototype.addListeners = function(){
+    if(this.playQueue !== null){
+        this.playQueue.addEventListener(
+            'playing',
+            this.onPlaying.bind(this),
+            false
+        );
+    }
+}
+
+AudioMetadataDisplay.prototype.onPlaying = function(e){
+     var song = e.target.song;
+     this.set(song);
+}
+
+AudioMetadataDisplay.prototype.set = function(song){
+    console.log('set', song);
+    this.setCoverart(song);
+    this.setTitle(song);
+    this.setArtist(song);
+    this.setAlbum(song);
+}
+
+AudioMetadataDisplay.prototype.setCoverart = function(song){
+     if(this.coverartEl){
+        if(song[this.songMap.coverart]){
+            var url = song[this.songMap.coverart];
+            this.coverartEl.css('background-image', 'url(' + url + ')');
+        }
+     }
+}
+
+AudioMetadataDisplay.prototype.setTitle = function(song){
+     if(this.titleEl){
+        if(song[this.songMap.title]){
+            var title = song[this.songMap.title];
+            this.titleEl.html(title);
+        }
+     }
+}
+
+AudioMetadataDisplay.prototype.setArtist = function(song){
+     if(this.artistEl){
+        if(song[this.songMap.artist]){
+            var artist = song[this.songMap.artist];
+            this.artistEl.html(artist);
+        }
+     }
+}
+
+AudioMetadataDisplay.prototype.setAlbum = function(song){
+     if(this.albumEl){
+        if(song[this.songMap.album]){
+            var album = song[this.songMap.album];
+            this.albumEl.html(album);
+        }
+     }
+}
+
+
+// check if we've got require
+if(typeof module !== "undefined"){
+    module.exports = AudioMetadataDisplay;
+}
+else{
+    window.ListSelector = AudioMetadataDisplay;
+}
+
+}()); // end wrapper
